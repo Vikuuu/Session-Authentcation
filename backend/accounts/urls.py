f1d7799bc3082f1aabfd03bb9090ad3975de2a3c
Aditya Vikram Singh <adivik672@gmail.com>
2023-10-02 23:01:24 +0530

    Login and Logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignupView, getCSRFToken
+from .views import SignupView, getCSRFToken, LoginView, LogoutView, CheckAuthenticatedView
 
 urlpatterns = [
+    path("authenticated", CheckAuthenticatedView.as_view()),
     path("register", SignupView.as_view()),
+    path("login", LoginView.as_view()),
+    path("logout", LogoutView.as_view()),
     path("csrf_cookie", getCSRFToken.as_view()),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,12 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from django.contrib import auth
 from user_profile.models import UserProfile
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+
+@method_decorator(csrf_protect, name="dispatch")
+class CheckAuthenticatedView(APIView):
+    def get(self, request, format=None):
+        try:
+            isAuthenticated = User.is_authenticated
+
+            if isAuthenticated:
+                return Response({"isAuthenticated": "success"})
+            else:
+                return Response({"isAuthenticated": "error"})
+        except:
+            return Response(
+                {"error": "Something went wrong when checking authentication status"}
+            )
 
 
 @method_decorator(csrf_protect, name="dispatch")
@@ -20,29 +37,65 @@ class SignupView(APIView):
         password = data["password"]
         re_password = data["re_password"]
 
-        if password == re_password:
-            if User.objects.filter(username=username).exists():
-                return Response({"error": "Username already exists"})
-            else:
-                if len(password) < 6:
-                    return Response({"error": "Password must be at least 6 characters"})
+        try:
+            if password == re_password:
+                if User.objects.filter(username=username).exists():
+                    return Response({"error": "Username already exists"})
                 else:
-                    user = User.objects.create_user(
-                        username=username, password=password
-                    )
+                    if len(password) < 6:
+                        return Response(
+                            {"error": "Password must be at least 6 characters"}
+                        )
+                    else:
+                        user = User.objects.create_user(
+                            username=username, password=password
+                        )
 
-                    user.save()
+                        user.save()
 
-                    user = User.objects.get(username=username)
+                        user = User.objects.get(id=user.id)
 
-                    user_profile = UserProfile(
-                        user.id, first_name="", last_name="", phone="", city=""
-                    )
-                    user_profile.save()
+                        user_profile = UserProfile(
+                            user=user, first_name="", last_name="", phone="", city=""
+                        )
+                        user_profile.save()
 
-                    return Response({"success": "User created successfully"})
-        else:
-            return Response({"error": "Passwords do not match"})
+                        return Response({"success": "User created successfully"})
+            else:
+                return Response({"error": "Passwords do not match"})
+        except:
+            return Response({"success": "Something went wrong when resistering"})
+
+
+@method_decorator(csrf_protect, name="dispatch")
+class LoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        data = request.data
+
+        username = data["username"]
+        password = data["password"]
+
+        try:
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return Response({"success": "User authenticated", "username": username})
+            else:
+                return Response({"error": "Error Authenticating"})
+        except:
+            return Response({"error": "something went wrong while logging in"})
+
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        try:
+            auth.logout(request)
+            return Response({"success": "Logout Out"})
+        except:
+            return Response({"error": "Something went wrong when logging out"})
 
 
 @method_decorator(ensure_csrf_cookie, name="dispatch")
